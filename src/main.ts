9e1c86eb2e7782b597ed525dcd1d59fcc9a2a6fc
leeskies <lee2dskies@gmail.com>
2024-02-28 21:54:43 +0200

    cors

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,6 @@ async function bootstrap() {
     origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     preflightContinue: false,
-    optionsSuccessStatus: 204,
     credentials: true,
   });
   await app.listen(3000);
